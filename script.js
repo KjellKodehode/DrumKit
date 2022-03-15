6fc9971dfdb14e97bba14a0296d577075d9adcc1
KjellKodeHode <kjell.arne.kodehode@gmail.com>
2022-03-15 14:46:08 +0100

    java work

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 numberOfButtons = document.querySelectorAll(".drum__btn").length;
 
-for (var i = 0; i < numberOfButtons; i++){
+for (let i = 0; i < numberOfButtons; i++){
 
 document.querySelectorAll("button")[i]
     .addEventListener("click", function(){
-        var buttonStyle = this.innerHTML;
+        let buttonStyle = this.innerHTML;
         sound(buttonStyle);
         animation(buttonStyle);
     });
@@ -18,37 +18,37 @@ document.addEventListener("keypress", function(event){
 function sound(key) {
     switch (key) {
         case "a":
-        var sound1 = new Audio("DrumSounds/clap.wav");
+        let sound1 = new Audio("DrumSounds/clap.wav");
         sound1.play();
         break;
     
         case "s":
-        var sound2 = new Audio("DrumSounds/hihat.wav");
+        let sound2 = new Audio("DrumSounds/hihat.wav");
         sound2.play();
         break;
     
         case "d":
-        var sound3 = new Audio('DrumSounds/kick.wav');
+        let sound3 = new Audio('DrumSounds/kick.wav');
         sound3.play();
         break;
     
         case "f":
-        var sound4 = new Audio('DrumSounds/openhat.wav');
+        let sound4 = new Audio('DrumSounds/openhat.wav');
         sound4.play();
         break;
     
         case "h":
-        var sound5 = new Audio('DrumSounds/ride.wav');
+        let sound5 = new Audio('DrumSounds/ride.wav');
         sound5.play();
         break;
     
         case "j":
-        var sound6 = new Audio('DrumSounds/snare.wav');
+        let sound6 = new Audio('DrumSounds/snare.wav');
         sound6.play();
         break;
     
         case "k":
-        var sound7 = new Audio('DrumSounds/tink.wav');
+        let sound7 = new Audio('DrumSounds/tink.wav');
         sound7.play();
         break;
 
@@ -62,7 +62,7 @@ function sound(key) {
     }
 
 function animation(currentKey) {
-    var activeButton = document.querySelector("." + currentKey);
+    let activeButton = document.querySelector("." + currentKey);
         
     activeButton.classList.add("animation_" + currentKey,);
     
